43cb8ff3948a1731d2e94a4d5b63357d96a7e0f9
Mincheol Kim <mcauto@naver.com>
2020-05-31 14:31:12 +0900

    chore: init project, add git hook pre-commit(ktlint, test)

---
+++ app/src/main/java/com/deo/todolist_mvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.deo.todolist_mvvm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
