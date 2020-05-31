dacec9f8b1a553c395ff1908adb299ea9095798f
Mincheol Kim <mcauto@naver.com>
2020-05-31 23:19:07 +0900

    ci: make gradle excutable (#4)
    * ci: make gradle excutable
    
    * chore: remove unuse gradle task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,3 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
-apply(from = rootProject.file("git-hooks.gradle"))
