f502073453be5e5a9bcc2f077bea52d282574e75
GitHub Action <action@github.com>
2020-06-05 15:46:03 +0000

    chore: update version code, name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.deo.todolist"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 6
-        versionName = "1.2.0"
+        versionCode = 7
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
