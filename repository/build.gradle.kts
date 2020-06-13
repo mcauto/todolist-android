c10974ac0d15faa106656e3c3c6fc2fab3feed5e
Mincheol Kim <mcauto@naver.com>
2020-06-13 10:44:47 +0900

    feat(repository): apply repository modularization with dagger2 (#10)
    * feat(repository): apply repository modularization with dagger2
    
    * feat(app): use repository module with coroutine
    
    * fix(ktlint): ignore import ordering
    
    https://github.com/pinterest/ktlint/issues/527
    
    * docs: modify readme
    
    * feat(ui): add username, password
    
    * fix: repository test, jdk11(x64), gradle jvm heap size up

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -2,24 +2,44 @@ plugins {
     id("java-library")
     id("kotlin")
     id("io.gitlab.arturbosch.detekt").version("1.9.1")
+    kotlin("kapt")
 }
+
 detekt {
     toolVersion = "1.9.1"
     config = files("../config/detekt/detekt.yml")
     buildUponDefaultConfig = true
 }
+
 apply(from = "../ktlint.gradle.kts")
+
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72")
-    implementation("com.google.code.gson:gson:2.8.5")
-    implementation("com.squareup.retrofit2:retrofit:2.8.1")
-    implementation("com.squareup.retrofit2:converter-gson:2.7.1")
+
     // Test
     testImplementation("junit:junit:4.13")
 
+    // Mockk
+    testImplementation("io.mockk:mockk:1.10.0")
+
+
+    // HTTP Client & Serializer
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
     // Coroutine
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
+
+    // Dependency Injection
+    val daggerVersion = "2.28"
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+    kapt("com.google.dagger:dagger-android-processor:$daggerVersion")
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    implementation("com.google.dagger:dagger-android:$daggerVersion")
+    implementation("com.google.dagger:dagger-android-support:$daggerVersion")
+
 
 }
